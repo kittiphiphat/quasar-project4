f81803f4b525a361041a8291df7bf2bad8531b60
kittiphiphat <makuth824@gmail.com>
2023-10-06 16:44:41 +0700

    test

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+  </div>
+  <InputPage/>
+  <FunctionPage/>
+
+</template>
+
+
+<script>
+import InputPage from "src/pages/InputPage.vue"
+import FunctionPage from "src/pages/FunctionPage.vue"
+export default {
+  name: 'HtmlComponent',
+  components:{
+    InputPage,
+    FunctionPage
+  },
+  data() {
+    return {
+      title: 'kittiphiphat toncharoen',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
